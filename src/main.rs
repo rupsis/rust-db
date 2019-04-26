a670776c9ded5f60e3326b12d5558a291766a0b9
Nate Rupsis <nathaniel.rupsis@homeadvisor.com>
2019-04-26 15:46:25 -0400

    added basic user input

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
+use std::io;
+use std::io::prelude::*;
+
+fn print_prompt() {
+    print!("db > ");
+    let _ = io::stdout().flush();
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting Rust DB");
 
     while(true){
-        println!("RustDB REPL")
+        print_prompt();
+
+        let mut user_input = String::new();
+        io::stdin().read_line(&mut user_input).expect("error: enable to read input");
+
+        println!("{}", user_input)
     }
 
 }
