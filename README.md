c344154b3f69c3192b4a3e29aab152e9433c7ef1
Nate Rupsis <nathaniel.rupsis@homeadvisor.com>
2019-04-27 22:05:42 -0400

    add basic meta command processing & terminal color

---
+++ README.md
@@ -0,0 +1,4 @@
+<img src="https://latex.codecogs.com/gif.latex? sqlite \; clone^2 " />
+
+
+Creating a Rust clone of @cstack [sqlite clone](https://github.com/cstack/db_tutorial) tutorial. 
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
 use std::io;
 use std::io::prelude::*;
+use colored::*;
+
+enum MetaCommandResult {
+    MetaCommandSuccess,
+    MetaCommandUnrecognized
+}
+
+enum PrepareResult {
+    PrepareSuccess,
+    PrepareUnrecognized
+}
+
 
 fn print_prompt() {
     print!("db > ");
     let _ = io::stdout().flush();
 }
+
+fn process_meta_command(command: &String) -> MetaCommandResult {
+     if (command == ".exit"){
+        std::process::exit(0);
+    } else {
+        return MetaCommandResult::MetaCommandUnrecognized;
+    }
+}
+
+// fn process_prepare_statement(command: &String) -> PrepareResult {
+// }
 
 fn main() {
     println!("Starting Rust DB");
@@ -14,15 +37,18 @@ fn main() {
 
         let mut user_input = String::new();
         io::stdin().read_line(&mut user_input).expect("error: enable to read input");
-
-        println!("{}", user_input);
-
+        
         user_input.truncate(user_input.len() - 1);
-        if (user_input == ".exit"){
-            std::process::exit(0);
-        } else {
-            println!("Unrecognized command {}", user_input)
+
+        if(&user_input[0..1] == "."){
+            match process_meta_command(&user_input) {
+                MetaCommandResult::MetaCommandSuccess => {
+                    println!("This is a meta command");
+                },
+                MetaCommandResult::MetaCommandUnrecognized => {
+                    println!("Unrecognized command {}", user_input.red());
+                },
+            }      
         }
     }
-
 }
